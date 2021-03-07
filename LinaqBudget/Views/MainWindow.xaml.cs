c009e685de3ba63db85db442aca08963d0e18443
Linaq <dominik.owsianka@gmail.com>
2021-03-07 12:48:43 +0100

    Added Serilog.

--- LinaqBudget/Views/MainWindow.xaml.cs
+++ LinaqBudget/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinaqBudget.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace LinaqBudget
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }

feac285032ccd68544b97538be255762224b5c78
Linaq <dominik.owsianka@gmail.com>
2021-03-07 10:52:19 +0100

    Base model added.

---
+++ LinaqBudget/Interfaces/IDataProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinaqBudget.Interfaces
+{
+    public interface IDataProvider
+    {
+        Transaction GetSingleTransactionById(int id);
+        List<Transaction> GetAllTransactions();
+        List<Transaction> GetTransactionsForAccount(Account account);
+        List<Transaction> GetTransactionsForCategory(Category category);
+        Account GetSingleAccountById(int id);
+        List<Account> GetAllAccounts();
+        Category GetSingleCategoryById(int id);
+        List<Category> GetAllCategories();
+    }
+}
